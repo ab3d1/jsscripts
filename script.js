ae7d69e0beada0cd4d1baeaa0981860aee38cebc
Abednego Ayienda <davenanony@gmail.com>
2024-07-23 14:02:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 userName = prompt("What is your name: ");
 
-alert("Hello " + userName.slice(0,1).toUpperCase() + userName.slice(1));
+alert("Hello " + userName.slice(0,1).toUpperCase() + userName.slice(1).toLowerCase());
